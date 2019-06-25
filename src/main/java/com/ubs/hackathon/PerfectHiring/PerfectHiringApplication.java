677608ce2453665e4cd627632e904e3b9b34a274
MukundPatel1809 <mukundp.786@gmail.com>
2019-06-25 12:28:43 +0530

    basing1.0- initial code setup

---
+++ src/main/java/com/ubs/hackathon/PerfectHiring/PerfectHiringApplication.java
@@ -0,0 +1,13 @@
+package com.ubs.hackathon.PerfectHiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfectHiringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfectHiringApplication.class, args);
+	}
+
+}
